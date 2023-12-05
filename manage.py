f7841d8cf85fc190faaa49cb3834c03120ef559a
Aiven Timptner <aiven@timptner.de>
2023-12-05 22:08:23 +0100

    Use generic entry-point

--- manage.py
+++ manage.py
@@ -1,59 +1,94 @@
 #! /usr/bin/env python3
 
+import time
 import unittest
 
 from argparse import ArgumentParser
+from importlib import import_module
 
-from tests import year2023
-from year2023 import day03, day04, day05
+from utilities.storage import read_data
+
+
+def add_default_args(parser: ArgumentParser) -> None:
+    parser.add_argument('year', type=int, choices=range(2016, 2024), help="Year of event")
+    parser.add_argument('day', type=int, choices=range(1, 26), help="Day of advent")
+
+
+def measure_execution_time(func, *args, **kwargs) -> (int, float):
+    start = time.time()
+    answer = func(*args, **kwargs)
+    stop = time.time()
+    delta = stop - start
+    return answer, delta
+
+
+def get_human_delta(delta: float) -> str:
+    if delta < 1:
+        return f"{delta * 1e3:.3f} ms"
+    else:
+        return f"{delta:.3f} s"
+
+
+def solve(args) -> None:
+    try:
+        module = import_module(f'year{args.year}.day{args.day:02d}')
+    except ModuleNotFoundError:
+        print(f"No solution for puzzle year {args.year} day {args.day}")
+        exit()
+
+    data = read_data(args.year, args.day, 'prod.txt')
+
+    answer, delta = measure_execution_time(module.first_part, data=data)
+    print(f"Part 1: {answer} ({get_human_delta(delta)})")
+
+    answer, delta = measure_execution_time(module.second_part, data=data)
+    print(f"Part 2: {answer} ({get_human_delta(delta)})")
+
+
+def test(args) -> None:
+    try:
+        module = import_module(f'tests.year{args.year}')
+    except ModuleNotFoundError:
+        print(f"No tests for puzzle year {args.year}")
+        exit()
+
+    try:
+        cls = getattr(module, f'TestDay{args.day:02d}')
+    except AttributeError:
+        print(f"No test case for puzzle year {args.year} day {args.day}")
+        exit()
+
+    suite = unittest.TestSuite()
+    result = unittest.TestResult()
+
+    test_first_part = cls('test_first_part')
+    test_second_part = cls('test_second_part')
+
+    suite.addTests([test_first_part, test_second_part])
+    suite.run(result=result)
+    if not result.wasSuccessful():
+        result.printErrors()
+        print(result.failures)
+        print("Tests failed!")
+    else:
+        print("Tests passed!")
 
 
 def main() -> None:
     parser = ArgumentParser()
 
-    parser.add_argument('command', choices=['test', 'solve'])
-    parser.add_argument('year', type=int, choices=range(2015, 2024))
-    parser.add_argument('day', type=int, choices=range(1, 26))
+    subparsers = parser.add_subparsers(title="Available commands")
+
+    parser_solve = subparsers.add_parser('solve', help="Solve the puzzle")
+    add_default_args(parser_solve)
+    parser_solve.set_defaults(func=solve)
+
+    parser_test = subparsers.add_parser('test', help="Test the puzzle")
+    add_default_args(parser_test)
+    parser_test.set_defaults(func=test)
 
     args = parser.parse_args()
-    year = args.year
-    day = args.day
-
-    if args.command == 'test':
-        tests = {
-            2023: {
-                3: year2023.TestDay03,
-                4: year2023.TestDay04,
-                5: year2023.TestDay05,
-            },
-        }
-        cls = tests[year][day]
-
-        suite = unittest.TestSuite()
-        result = unittest.TestResult()
-
-        test_first_part = cls('test_first_part')
-        test_second_part = cls('test_second_part')
-
-        suite.addTests([test_first_part, test_second_part])
-        suite.run(result=result)
-        if not result.wasSuccessful():
-            result.printErrors()
-            print(result.failures)
-            print("Tests failed!")
-        else:
-            print("Tests passed!")
-
-    if args.command == 'solve':
-        solutions = {
-            2023: {
-                3: day03,
-                4: day04,
-                5: day05,
-            },
-        }
-        func = solutions[year][day]
-        func.main()
+    args.func(args)
 
 
 if __name__ == '__main__':
