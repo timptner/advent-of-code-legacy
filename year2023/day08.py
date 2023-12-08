830dfd5558264e617ec2e72d200e2c1872704f51
Aiven Timptner <aiven@timptner.de>
2023-12-08 16:17:28 +0100

    Remove force brute from second part of solution for year 2023 day 8

--- year2023/day08.py
+++ year2023/day08.py
@@ -1,65 +1,62 @@
-def first_part(data: str) -> int:
+import math
+
+
+def parse_data(data: str) -> tuple[list[int], dict[str, tuple[str, str]]]:
     instructions, nodes = data.split('\n\n')
+
+    converter = {
+        'L': 0,
+        'R': 1,
+    }
+    instructions = [converter[instruction] for instruction in instructions]
 
     network = {}
     for line in nodes.splitlines():
         key, value = line.replace(' ', '').split('=')
         left, right = value[1:-1].split(',')
-        network[key] = {'L': left, 'R': right}
+        network[key] = (left, right)
 
+    return instructions, network
+
+
+def get_steps(instructions: list[int], network: dict[str, tuple[str, str]],
+              start_node: str, end_node: list[str]) -> int:
     length = len(instructions)
-    current_node = 'AAA'
+    current_node = start_node
     counter = 0
     while True:
         index = counter % length
         instruction = instructions[index]
         next_node = network[current_node][instruction]
-        if length < 10:
-            print(f"[{counter:06d}] {current_node} -> {next_node}")
-        if next_node == 'ZZZ':
+        if next_node in end_node:
             break
         else:
             counter += 1
             current_node = next_node
-
     return counter + 1
+
+
+def first_part(data: str) -> int:
+    instructions, network = parse_data(data)
+    steps = get_steps(instructions, network, 'AAA', ['ZZZ'])
+    return steps
 
 
 def second_part(data: str) -> int:
-    instructions, nodes = data.split('\n\n')
+    instructions, network = parse_data(data)
 
-    network = {}
-    for line in nodes.splitlines():
-        key, value = line.replace(' ', '').split('=')
-        left, right = value[1:-1].split(',')
-        network[key] = {'L': left, 'R': right}
+    start_nodes = []
+    end_nodes = []
+    for key in network.keys():
+        if key.endswith('A'):
+            start_nodes.append(key)
+        if key.endswith('Z'):
+            end_nodes.append(key)
 
-    current_nodes = [key for key in network.keys() if key.endswith('A')]
-    amount = len(current_nodes)
-    print(f"Doing {amount} path findings in parallel (still as sequential process...)")
+    list_of_steps = []
+    for start_node in start_nodes:
+        steps = get_steps(instructions, network, start_node, end_nodes)
+        list_of_steps.append(steps)
 
-    length = len(instructions)
-    counter = 0
-    while True:
-        if counter % 10**6 == 0:
-            print(counter)
-        index = counter % length
-        instruction = instructions[index]
-        ends = []
-        next_nodes = []
-        for node in current_nodes:
-            next_node = network[node][instruction]
-            next_nodes.append(next_node)
-            if next_node.endswith('Z'):
-                ends.append(True)
-            else:
-                ends.append(False)
-        if length < 10:
-            print(f"[{counter:06d}] {current_nodes} -> {next_nodes}")
-        if all(ends):
-            break
-        else:
-            counter += 1
-            current_nodes = next_nodes
-
-    return counter + 1
+    total_steps = math.lcm(*list_of_steps)
+    return total_steps
